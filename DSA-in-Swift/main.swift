f33a9d81a90d64f2ad64ad740ba0cfed4183e765
Thanveer ahmed Shaik <shaikthanveerahmed123@gmail.com>
2025-08-01 00:02:53 +0300

    Initial commit: Add DSA in Swift project

---
+++ DSA-in-Swift/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  DSA-in-Swift
+//
+//  Created by Thanveer ahmed Shaik on 31/07/25.
+//
+
+import Foundation
+
+let number = 153
+if isArmstrong(number) {
+    print("\(number) is an Armstrong number.")
+} else {
+    print("\(number) is not an Armstrong number.")
+}
+
